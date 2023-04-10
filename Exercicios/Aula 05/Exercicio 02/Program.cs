fa8e2213ccaa63eda20b51d11a2e7a322bca7421
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-10 20:17:19 -0300

    Adicionar exercício 02 da aula 05

---
+++ Exercicios/Aula 05/Exercicio 02/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 02 - Faça um algoritmo que leia 10 números pelo teclado, e que no final mostre o maior deles. Não usar vetor.
+            
+            int maior = 0; //variável para armazenar o maior número digitado
+            int num; //variável para armazenar cada número digitado pelo usuário
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Digite o número " +i);
+                num = int.Parse(Console.ReadLine());
+
+                if (num > maior)
+                {
+                    maior = num; //Atualiza o valor de 'maior' se o número digitado for maior
+                }
+            }
+            Console.WriteLine("O maior número digitado foi: " + maior);
+            
+
+
+
+
+        }
+    }
+}
