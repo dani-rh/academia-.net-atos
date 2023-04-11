b84455e8eb4bdd15622bfbe203c0e88bc284a3ec
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-11 15:11:44 -0300

    Adicionar exercício 07 da aula 05

---
+++ Exercicios/Aula 05/Exercicio 07/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //7 Escreva um programa em C# leia 20 números e que conte a quantidade de números pares e ímpares digitados por um usuário. Apresente o resultado.
+
+            int n, i, par = 0, impar = 0;
+
+            for (i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Digite um número: ");
+                n = int.Parse(Console.ReadLine());
+                if (n % 2 == 0)
+                {
+                    par++;
+                }
+                else
+                {
+                    impar++;
+                }
+            }
+            Console.WriteLine("Foram digitados " + par + " números pares.");
+            Console.WriteLine("Foram digitados " +impar+ " números ímpares."); ;
+        }
+    }
+}
