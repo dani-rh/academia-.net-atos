fae76402b66319f4bd73e61334659fae6ac7f807
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-11 15:11:29 -0300

    Adicionar exercício 05 da aula 05

---
+++ Exercicios/Aula 05/Exercicio 05/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercício 05 - Escreva um algoritmo para calcular o fatorial de um número.
+
+            Console.WriteLine("Digite um número inteiro positivo: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            int fatorial = 1;
+
+            for (int i = 1; i <= numero; i++)
+            {
+                fatorial *= i; 
+            }
+
+            Console.WriteLine("O fatorial de " + numero + " é igual a " + fatorial + ".");
+
+        }
+    }
+}
