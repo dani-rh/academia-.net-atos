4a323f9da7761bfa101085ddea4e31b7c3285df9
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-11 15:11:24 -0300

    Adicionar exercício 04 da aula 05

---
+++ Exercicios/Aula 05/Exercicio 04/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //4 - Escreva um algoritmo que leia dois valores pelo teclado, x e y, e em seguida calcule a potência de x elevado na y sem utilizar a função pow.
+
+            Console.WriteLine("Digite um valor para x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um valor para y: ");
+            int y = int.Parse(Console.ReadLine());
+
+            int resultado = 1;
+
+            for (int i = 0; i < y; i++) 
+            {
+                resultado *= x;
+            }
+
+            Console.WriteLine("O resultado de " + x + " elevado a " + y + " é " + resultado+ ".");
+
+        }
+    }
+}
