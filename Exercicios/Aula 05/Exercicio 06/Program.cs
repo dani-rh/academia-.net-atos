d2bdc54dbe5167b7e9aacf03e90a59fcbe510eb6
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-11 15:11:36 -0300

    Adicionar exercício 06 da aula 05

---
+++ Exercicios/Aula 05/Exercicio 06/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercicio_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercício 06 - Escreva um algoritmo que leia o número de alunos de uma turma e em seguida leia a quantidade de avaliações aplicadas para aquela turma.
+            //Por fim, leia as notas das avaliações de cada aluno e mostre sua nota final (média das notas das avaliações).
+
+            int alunos, quantAvalia, i, j;
+            double notaAluno, somaNota = 0, media = 0;
+
+            Console.WriteLine("Digite a quantidade de alunos: ");
+            alunos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de avaliações: ");
+            quantAvalia = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < alunos; i++)
+            {
+                for (j = 0; j < quantAvalia; j++)
+                {
+                    Console.WriteLine("Digite a nota " +(j+1)+ " do aluno " +(i+1));
+                    notaAluno = double.Parse(Console.ReadLine());
+                    somaNota += notaAluno;
+                    somaNota = 0;
+                }
+                media = somaNota / quantAvalia;
+                Console.WriteLine("A média do aluno " +(i+j)+ " = " +media);
+                media = 0;
+            }
+
+
+
+        }
+    }
+}
