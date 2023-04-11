bad5f63809849e83ae7f3e33aa4e1a0b4ffa522e
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-11 17:18:18 -0300

    Adicionar exercício 01 da aula 06

---
+++ Exercicios/Aula 06/Exercicio 01/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.Faça um programa em VS que solicite um número inteiro positivo ao usuário, validando a entrada de dados
+            //(informando se ele estiver errado e repetindo a solicitação até que esteja correto). 
+            //Após o programa em VS deve informar todos os números pares existentes entre 1 e o número fornecido pelo usuário.
+
+            int numero = 0;
+            for (;numero <= 0 ;)
+            {
+                Console.WriteLine("Digite um número inteiro positivo: ");
+                if (!int.TryParse(Console.ReadLine(), out numero) || numero <= 0)
+                {
+                    Console.WriteLine("Valor incorreto!");
+                }
+            }
+            Console.WriteLine("Número digitado: "+numero);
+            Console.Write("Números inteiros pares entre 1 e " + numero+ ": ");
+            for (int i = 1; i < numero; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i+ ", ");
+                }
+            }
+        }
+    }
+}
