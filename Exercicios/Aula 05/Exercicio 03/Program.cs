e86a01960d0ed9641a32991e7093c69598b850f3
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-11 15:11:16 -0300

    Adicionar exercício 03 da aula 05

---
+++ Exercicios/Aula 05/Exercicio 03/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escreva um algoritmo que leia pelo teclado um valor e então calcule a tabuada de 1 a 10 de um número x lido utilizando laços de repetição, e mostre na tela.
+
+            Console.WriteLine("Digite um valor: ");
+            int x = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i <= 10; i++)
+            {
+                int multi = x * i;
+                Console.WriteLine(x+ " X " +i+ " = " + multi);
+            }
+        }
+    }
+}
