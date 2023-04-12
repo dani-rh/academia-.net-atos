3d25ea7a35ae0dc0560cafb590e2e249f104dc79
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-12 17:06:52 -0300

    Adicionar exercicio 12 da aula 06

---
+++ Exercicios/Aula 06/Exercicio 12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Runtime.Serialization;
+
+namespace Exercicio_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //12.Apresentar o total da soma dos cem primeiros números inteiros.
+
+            int soma = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                soma += i;
+            }
+            Console.WriteLine("Soma dos cem primeiros números inteiros: " + soma);
+        }
+    }
+}
