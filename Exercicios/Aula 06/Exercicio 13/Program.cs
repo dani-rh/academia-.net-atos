2185da842f4ee61d6a58a79cd55b9673af4bae7a
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-12 17:23:25 -0300

    Adicionar exercicio 13 da aula 06

---
+++ Exercicios/Aula 06/Exercicio 13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace Exercicio_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //13.Elaborar um programa que apresente o somatório dos valores pares existentes entre 1 e 500.
+
+            int soma = 0;
+            
+            for (int i = 1; i < 500; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                    
+                    soma += i;
+                }
+            }
+            Console.WriteLine("Somatório dos valores pares existentes entre 1 e 500: " + soma);
+        }
+    }
+}
