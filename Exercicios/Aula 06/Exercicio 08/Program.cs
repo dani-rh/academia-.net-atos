cab6f97761e326c4efef3242e142795f9fea070c
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-11 22:24:06 -0300

    Adicionar exercício 08 da aula 06

---
+++ Exercicios/Aula 06/Exercicio 08/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Exercicio_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //8. Faça um programa em VS que solicite um numero inteiro. Se o numero não for inteiro, solicite-o até que seja. Após, informe se ele é par ou impar.
+            //Finalmente, pergunte ao usuário se ele quer repetir o programa em VS.
+            
+            bool repetir = true;
+
+            while (repetir)
+            {
+                int numero = 0;
+                for (; numero <= 0;)
+                {
+                    Console.WriteLine("Digite um número inteiro: ");
+                    if (!int.TryParse(Console.ReadLine(), out numero) || numero <= 0)
+                    {
+                        Console.WriteLine("Valor incorreto!");
+                    }
+                }
+
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("O número " + numero + " é par.");
+                }
+                else
+                {
+                    Console.WriteLine("O número " + numero + " é ímpar.");
+                }
+                
+                Console.WriteLine("\nDeseja repetir o programa (S/N)? ");
+                string resposta = Console.ReadLine().ToUpper();
+
+                if (resposta == "N")
+                {
+                    repetir = false;
+                }
+            }
+
+        }
+    }
+}
