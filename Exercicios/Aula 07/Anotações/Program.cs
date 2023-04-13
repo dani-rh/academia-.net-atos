7fbd0d96656d31f4cd892d487100136285d1ffdb
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-13 15:26:11 -0300

    Adicionar anotações da aula 07

---
+++ Exercicios/Aula 07/Anotações/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Anotações
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[3];
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine("Informe um valor para a posição " + i + ":");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine("O vetor possui na posição " + i + " o valor " + vetor[i]);
+                Console.WriteLine("vetor[" + i + "] = " + vetor[i]);
+            }
+
+
+        }
+    }
+}
