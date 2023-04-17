c48ac981033d24a56e9cdae5cfe1a747db55f7ba
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-17 19:05:12 -0300

    Adicionar exercício 09 da aula 08

---
+++ Exercicios/Aula 08/Exercicio 09/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Exercicio_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercício 09 - Leia uma matriz 3x3. Em seguida, solicite um número qualquer ao usuário e pesquise
+            //na matriz se o número existe.  
+            //Caso, seja verdade imprima a mensagem:  “O número existe no vetor”
+            //caso contrário “Número inexistente”.
+
+            //Populando a matriz 3x3 com valores aleatórios
+
+            int[,] matriz = new int[3, 3];
+            Random random = new Random();
+
+            Console.WriteLine("Matriz : ");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matriz[i, j] = random.Next(0, 50);
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            //Solicitando um número qualquer ao usuário
+            Console.WriteLine("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            //Checando se o número existe na matriz:
+            bool existe = false;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (matriz[i,j] == numero)
+                    {
+                        existe = true;
+                        break;
+                    }
+                }
+            }
+
+            //Imprimindo a mensagem solicitada
+            if (existe)
+            {
+                Console.WriteLine("O número existe no vetor.");
+            }
+            else
+            {
+                Console.WriteLine("Número inexistente.");
+            }
+
+        }
+    }
+}
