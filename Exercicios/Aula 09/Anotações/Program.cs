3185f9770b70d28fbc65dc9bb95bbef3ee0d9080
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-18 16:53:24 -0300

    Adicionar Anotações da aula 09

---
+++ Exercicios/Aula 09/Anotações/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Anotações
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            static double subtrai(double num1, double num2)
+            {
+                double resultado;
+                resultado = num1 - num2;
+                return resultado;
+            }
+            static void soma(double num1, double num2)
+            {
+                double resultado;
+                resultado = num1 + num2;
+                Console.WriteLine(num1 + "+" + num2 + "=" + resultado);
+            }
+            static double divide(double num1, double num2)
+            {
+                double resultado;
+                resultado = num1 / num2;
+                return resultado;
+            }
+            static double multiplica(double num1, double num2)
+            {
+                double resultado;
+                resultado = num1 * num2;
+                return resultado;
+            }
+            static bool verificaPar(double numero)
+            {
+                if (numero % 2 == 0)
+                    return true;
+                else return false;
+            }
+
+            static void Main(string[] args)
+            {
+                double n1, n2, res;
+                Console.WriteLine("Digite dois numeros: ");
+                n1 = double.Parse(Console.ReadLine());
+                n2 = double.Parse(Console.ReadLine());
+                soma(n1, n2);
+                res = subtrai(n1, n2);
+                Console.WriteLine(n1 + "-" + n2 + "=" + res);
+                res = multiplica(n1, n2);
+                Console.WriteLine(n1 + "*" + n2 + "=" + res);
+                Console.WriteLine(n1 + "/" + n2 + "=" + divide(n1, n2));
+                if (verificaPar(n1))
+                    Console.WriteLine("Par");
+                else Console.WriteLine("Impar");
+                if (verificaPar(n2))
+                    Console.WriteLine("Par");
+                else Console.WriteLine("Impar");
+            }
+
+        }
+    }
+}
