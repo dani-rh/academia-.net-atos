3cf1d658e85a561ce8a95c27ce7c3ec180d1160a
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-23 23:27:19 -0300

    Atualizar exercício avaliativo

--- Avaliações/Avaliativo  01/Program.cs
+++ Avaliações/Avaliativo  01/Program.cs
@@ -24,179 +24,156 @@ namespace Avaliativo__01
             //Forma e envio: Enviar pelo chat o link do github
 
             //Declara a função do tabuleiro
-            char[,] tabuleiro = new char[3, 3];
-            { for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        tabuleiro[i, j] = ' ';
-                    }
-                }
-            }
+            bool repetir = true;
 
-            //Declara o jogador atual para determinar qual jogador deve jogar na próxima rodada
-            char jogadorAtual = 'X';
-
-            while (!VerificaFimDeJogo(tabuleiro))
+            while (repetir)
             {
-                //Limpa o console e imprime o tabuleiro atualizado
-                Console.Clear();
-                ImprimeTabuleiro(tabuleiro);
+                Console.WriteLine("Bem vindo ao jogo da velha!\n");
+                Thread.Sleep(2000);
 
-                Console.WriteLine("Jogador " + jogadorAtual + " insira a posição do tabuleiro i,j: ");
-                string[] jogada = Console.ReadLine().Split(','); //String inserida pelo jogador é dividida em duas partes usando a vírgula como separador
-                int linha;
-                int coluna;
-
-                //Verifica se a posição é existente na matriz e retorna a jogada nas variáveis linha e coluna
-                if (jogada.Length != 2 || !int.TryParse(jogada[0], out linha) || !int.TryParse(jogada[1], out coluna) || linha < 0 || linha > 2 || coluna < 0 || coluna > 2)
-                {
-                    Console.WriteLine("Essa posição não existe! Tente novamente. ");
-                    continue;
-                }
-
-                //Verifica se a posição já foi preenchida anteriormente
-                if (tabuleiro[linha, coluna] != ' ')
-                {
-                    Console.WriteLine("Posição já preenchida! Tente novamente. ");
-                    continue;
-                }
-
-                tabuleiro[linha, coluna] = jogadorAtual;
-                jogadorAtual = (jogadorAtual == 'X') ? 'O' : 'X';
-            }
-
-            Console.Clear();
-            ImprimeTabuleiro(tabuleiro);
-
-            //Função para verificar o vencedor 
-            char vencedor = VerificaVencedor(tabuleiro);
-            if (vencedor == ' ')
-            {
-                Console.WriteLine("Empate!");
-            }
-            else
-            {
-                Console.WriteLine("Jogador " + vencedor + " venceu!");
-            }
-
-            //Imprimir tabuleiro com as posições
-            static void ImprimeTabuleiro(char[,] tabuleiro)
-            {
-                for (int i = 0; i < 1; i++)
-                {
-                    for (int j = 0; j < 3; j++)
+                char[,] tabuleiro = new char[3, 3];
+                { for (int i = 0; i < 3; i++)
                     {
-                        Console.Write(" ({0}, {1}) | ", i, j);
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine("________|_________|_________");
-                for (int i = 1; i < 2; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        Console.Write(" ({0}, {1}) | ", i, j);
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine("________|_________|_________");
-                for (int i = 2; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        Console.Write(" ({0}, {1}) | ", i, j);
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine("        |         |        ");
-            }
-            //    Console.WriteLine(" {0} | {1} | {2} ", tabuleiro[0, 0], tabuleiro[0, 1], tabuleiro[0,2]);
-            //    Console.WriteLine("___|___|___");
-            //    Console.WriteLine(" {0} | {1} | {2} ", tabuleiro[1, 0], tabuleiro[1, 1], tabuleiro[1, 2]);
-            //    Console.WriteLine("___|___|___");
-            //    Console.WriteLine(" {0} | {1} | {2} ", tabuleiro[2, 0], tabuleiro[2, 1], tabuleiro[2, 2]);
-
-            //{
-            //    for (int i = 0; i < 1; i++)
-            //    {
-            //        for (int j = 0; j < 3; j++)
-            //        {
-            //            Console.Write("({0},{1})|", i, j);
-            //        }
-            //        Console.WriteLine();
-            //    }
-            //    Console.WriteLine("_____|_____|_____");
-            //    for (int i = 1; i < 2; i++)
-            //    {
-            //        for (int j = 0; j < 3; j++)
-            //        {
-            //            Console.Write("({0},{1})|", i, j);
-            //        }
-            //        Console.WriteLine();
-            //    }
-            //    Console.WriteLine("_____|_____|_____");
-            //    for (int i = 2; i < 3; i++)
-            //    {
-            //        for (int j = 0; j < 3; j++)
-            //        {
-            //            Console.Write("({0},{1})|", i, j);
-            //        }
-            //        Console.WriteLine();
-            //    }
-            //    Console.WriteLine("     |     |     ");
-            //}
-
-            static bool VerificaFimDeJogo(char[,] tabuleiro)
-            {
-                return VerificaVencedor(tabuleiro) != ' ' || VerificaEmpate(tabuleiro);
-            }
-
-            static char VerificaVencedor(char[,] tabuleiro)
-            {
-                //Verificar se há um vencedor na linha (i)
-                for (int i = 0; i < 3; i++)
-                {
-                    if (tabuleiro[i, 0] == tabuleiro[i, 1] && tabuleiro[i, 1] == tabuleiro[i, 2] && tabuleiro[i, 0] != ' ')
-                    {
-                        return tabuleiro[i, 0];
-                    }
-                }
-                //Verificar se há um vencedor na coluna (j)
-                for (int j = 0; j < 3; j++)
-                {
-                    if (tabuleiro[0, j] == tabuleiro[1, j] && tabuleiro[1, j] == tabuleiro[2, j] && tabuleiro[0, j] != ' ')
-                    {
-                        return tabuleiro[0, j];
-                    }
-                }
-                //Verificar se há um vencedor na diagonal principal
-                if (tabuleiro[0, 0] == tabuleiro[1, 1] && tabuleiro[1, 1] == tabuleiro[2, 0] && tabuleiro[0, 2] != ' ')
-                {
-                    return tabuleiro[0, 2];
-                }
-                //Se não houver vencedor, retorna espaço em branco
-                return ' ';
-            }
-
-            static bool VerificaEmpate(char[,] tabuleiro)
-            {
-                //Verifica se todas as posições estão preenchidas
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        if (tabuleiro[i, j] == ' ')
+                        for (int j = 0; j < 3; j++)
                         {
-                            return false;
+                            tabuleiro[i, j] = ' ';
                         }
                     }
                 }
 
-                //Se todas as posições estiverem preenchidas e não houver vencedor é empate
-                return true;
-            }
+                //Declara o jogador atual para determinar qual jogador deve jogar na próxima rodada
+                char jogadorAtual = 'X';
 
+                while (!VerificaFimDeJogo(tabuleiro))
+                {
+                    //Limpa o console e imprime o tabuleiro atualizado
+                    Console.Clear();
+                    ImprimeTabuleiro(tabuleiro);
+
+                    Console.WriteLine("\nJogador " + jogadorAtual + " insira a posição do tabuleiro x,y: ");
+                    string[] jogada = Console.ReadLine().Split(','); //String inserida pelo jogador é dividida em duas partes usando a vírgula como separador (split)
+                    int linha;
+                    int coluna;
+
+                    //Verifica se a posição é existente na matriz e retorna a jogada nas variáveis linha e coluna
+                    if (jogada.Length != 2 || !int.TryParse(jogada[0], out linha) || !int.TryParse(jogada[1], out coluna) || linha < 0 || linha > 2 || coluna < 0 || coluna > 2)
+                    {
+                        Console.WriteLine("\nEssa posição não existe! Tente novamente. ");
+                        Thread.Sleep(2000);
+                        continue;
+                    }
+
+                    //Verifica se a posição já foi preenchida anteriormente
+                    if (tabuleiro[linha, coluna] != ' ')
+                    {
+                        Console.WriteLine("\nPosição já preenchida! Tente novamente. ");
+                        Thread.Sleep(2000);
+                        continue;
+                    }
+
+                    tabuleiro[linha, coluna] = jogadorAtual;
+                    jogadorAtual = (jogadorAtual == 'X') ? 'O' : 'X';
+
+                }
+
+                Console.Clear();
+
+                //Função para verificar o vencedor 
+                char vencedor = VerificaVencedor(tabuleiro);
+                if (vencedor == ' ')
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    ImprimeTabuleiro(tabuleiro);
+                    Console.WriteLine("\nEmpate!");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    ImprimeTabuleiro(tabuleiro);
+                    Console.WriteLine("\nJogador " + vencedor + " venceu!");
+                }
+                Console.ResetColor();
+
+                //Imprimir tabuleiro com as posições
+
+                static void ImprimeTabuleiro(char[,] tabuleiro)
+                {
+                    Console.WriteLine("  0,0  |  0,1  |  0,2  ");
+                    Console.WriteLine("   {0}   |   {1}   |  {2} ", tabuleiro[0, 0], tabuleiro[0, 1], tabuleiro[0, 2]);
+                    Console.WriteLine("_______|_______|_______");
+                    Console.WriteLine("  1,0  |  1,1  |  1,2  ");
+                    Console.WriteLine("   {0}   |   {1}   |  {2} ", tabuleiro[1, 0], tabuleiro[1, 1], tabuleiro[1, 2]);
+                    Console.WriteLine("_______|_______|_______");
+                    Console.WriteLine("  2,0  |  2,1  |  2,2  ");
+                    Console.WriteLine("   {0}   |   {1}   |  {2} ", tabuleiro[2, 0], tabuleiro[2, 1], tabuleiro[2, 2]);
+                }
+
+                static bool VerificaFimDeJogo(char[,] tabuleiro)
+                {
+                    return VerificaVencedor(tabuleiro) != ' ' || VerificaEmpate(tabuleiro);
+                }
+
+                static char VerificaVencedor(char[,] tabuleiro)
+                {
+                    //Verificar se há um vencedor na linha (i)
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (tabuleiro[i, 0] == tabuleiro[i, 1] && tabuleiro[i, 1] == tabuleiro[i, 2] && tabuleiro[i, 0] != ' ')
+                        {
+                            return tabuleiro[i, 0];
+                        }
+                    }
+
+                    //Verificar se há um vencedor na coluna (j)
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (tabuleiro[0, j] == tabuleiro[1, j] && tabuleiro[1, j] == tabuleiro[2, j] && tabuleiro[0, j] != ' ')
+                        {
+                            return tabuleiro[0, j];
+                        }
+                    }
+
+                    //Verificar se há um vencedor na diagonal principal
+                    if (tabuleiro[0, 0] == tabuleiro[1, 1] && tabuleiro[1, 1] == tabuleiro[2, 2] && tabuleiro[2, 2] != ' ')
+                    {
+                        return tabuleiro[0, 0];
+                    }
+
+                    //Verificar se há um vencedor na diagonal secundária
+                    if (tabuleiro[0, 2] == tabuleiro[1, 1] && tabuleiro[1, 1] == tabuleiro[2, 0] && tabuleiro[0, 2] != ' ')
+                    {
+                        return tabuleiro[0, 2];
+                    }
+
+                    //Se não houver vencedor, retorna espaço em branco
+                    return ' ';
+                }
+
+                static bool VerificaEmpate(char[,] tabuleiro)
+                {
+                    //Verifica se todas as posições estão preenchidas
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (tabuleiro[i, j] == ' ')
+                            {
+                                return false;
+                            }
+                        }
+                    }
+
+                    //Se todas as posições estiverem preenchidas e não houver vencedor é empate
+                    return true;
+                }
+
+                Console.WriteLine("\nDeseja recomeçar o jogo (S/N)? ");
+                string reiniciarJogo = Console.ReadLine().ToUpper();
+                if (reiniciarJogo == "N")
+                {
+                    repetir = false;
+                }
+            }
         }
     }
 }
