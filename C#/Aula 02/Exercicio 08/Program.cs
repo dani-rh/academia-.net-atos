9fc81dbc34335005388c0d71f53901717d5a3eae
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-26 14:32:36 -0300

    Adicionar exercicio 08 aula 02

---
+++ C#/Aula 02/Exercicio 08/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercicio_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercício 08 - Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. 
+            //A fórmula da conversão é F = (9 * C + 160) / 5.
+                        Console.WriteLine("Digite uma temperatura em Celsius: ");
+            double celsius = double.Parse(Console.ReadLine());
+            double fahrenheit = (9 * celsius + 160) / 5;
+            Console.WriteLine("A temperatura em Fahrenheit é: " + fahrenheit);
+        }
+    }
+}
