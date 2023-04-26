c3e129b6b34b6a8abb268e701b51429f1b8fdc25
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-26 14:31:58 -0300

    Adicionar exercicio 03 aula 02

---
+++ C#/Aula 02/Exercicio 03/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercício 03 - Escreva a seqüência de atribuições, em forma de programa em C# e no Visual Studio, para trocar entre 
+            // si os valores de duas variáveis A e B.
+
+            Console.WriteLine("Digite um valor para A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um valor para B: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("O valor de A é: " + a + " e " + "o valor de B é: " + b);
+        }
+    }
+}
