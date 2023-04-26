2840b36fa4302b76a2d73449542f9e7116111263
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-04-26 14:31:44 -0300

    Adicionar exercicio 01 aula 02

---
+++ C#/Aula 02/Exercicio 01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio_01_use
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercício 01 - Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre dois números quaisquer.
+
+            double a;
+            double b;
+            Console.WriteLine("Digite um valor para A: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor para B: ");
+            b = double.Parse(Console.ReadLine());
+            double media = (a + b) / 2;
+            Console.WriteLine("A média aritmética é: " + media);
+        }
+    }
+}
