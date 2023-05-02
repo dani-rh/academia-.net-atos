154137d526f2df7db0353de46730e3b499ddc5c0
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-02 17:03:12 -0300

    Adicionar exemplo 01 aula 10

---
+++ C#/Aula 10/Exemplo 01/Carro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_01
+{
+    internal class Carro
+    {
+        public string marca;
+        public string modelo;
+        public string anoFabricacao;
+        public void exibeDados()
+        {
+            Console.WriteLine("Marca: "+this.marca);
+            Console.WriteLine("Marca: "+this.modelo);
+            Console.WriteLine("Marca: "+this.anoFabricacao);
+        }
+
+
+    }
+}
---
+++ C#/Aula 10/Exemplo 01/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exemplo_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mar, mo;
+            int ano;
+
+            Carro c1 = new Carro();
+            Carro c2 = new Carro();
+            
+            Console.WriteLine("Digite a marca do carro: ");
+            c1.marca = Console.ReadLine();
+            Console.WriteLine("Digite o modelo do carro: ");
+            c1.modelo = Console.ReadLine();
+            Console.WriteLine("Digite o ano de fabricação: ");
+            c1.anoFabricacao = Console.ReadLine();
+
+            Console.WriteLine("Digite a marca do carro: ");
+            c2.marca = Console.ReadLine();
+            Console.WriteLine("Digite o modelo do carro: ");
+            c2.modelo = Console.ReadLine();
+            Console.WriteLine("Digite o ano de fabricação: ");
+            c2.anoFabricacao = Console.ReadLine();
+
+            Console.WriteLine("Carro 1: ");
+            c1.exibeDados();
+            Console.WriteLine("Carro 2: ");
+            c2.exibeDados();
+
+        }
+    }
+}
