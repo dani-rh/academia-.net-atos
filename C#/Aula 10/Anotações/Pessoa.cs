41ae561301632dff93573a58769e265b7bb6c272
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-02 15:19:38 -0300

    Adicionar nova aula

---
+++ C#/Aula 10/Anotações/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anotações
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public int idade;
+
+        public Pessoa()
+        {
+
+        }
+
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            //para receber aquilo que já defini como parametro - Snow, 6 anos
+        }
+
+        public void exibeDados()
+        {
+            Console.WriteLine("Nome: "+this.nome);
+            Console.WriteLine("Idade: "+this.idade);
+        }
+    }
+}
---
+++ C#/Aula 10/Anotações/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+using System.Text.RegularExpressions;
+
+namespace Anotações
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, estou no primeiro programa 00");
+            Pessoa p = new Pessoa();//instanciando o objeto
+
+            Console.WriteLine("Digite o nome da pessoa: ");
+            p.nome = Console.ReadLine();//aqui estou atribuindo nome
+
+            Console.WriteLine("Digite a idade da pessoa: ");
+            p.idade = int.Parse(Console.ReadLine()); //aqui estou atribuindo idade
+
+            //abaixo, estou mostrando os dados?
+            //Console.WriteLine("A pessoa instanciada é: "+p.nome);
+            //Console.WriteLine("E tem "+p.idade+" anos");
+            p.exibeDados();
+
+            Pessoa p2 = new Pessoa("Snow", 6);
+            p2.exibeDados();
+
+            //Exemplo 01 - 
+            //Crie uma classe chamada Carro que possua 3 atributos: marca, modelo e anoFabricacao.Além disso, na main crie 2 objetos do tipo Carro e exiba na tela os valores dos atributos criados.
+            //Esses valores devem ser solicitados ao usuário, por linha de execução.
+
+            
+        }
+    }
+}
