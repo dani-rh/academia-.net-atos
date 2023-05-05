8ce302c955b93e677e57fe7419bcb4b93ae86eb9
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-05 17:32:00 -0300

    Adicionar aula 05.05

---
+++ C#/11. Aula 05.05/Exercicio 02/ProfissionalSaude.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_02
+{
+    internal class ProfissionalSaude
+    {
+        //3 elementos que ao mesmo tempo são métodos get and set
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public string RegistroConselho { get; set; }
+
+        public ProfissionalSaude() { }
+
+        public ProfissionalSaude(string nome, string tipo, string registroConselho)
+        {
+            Nome = nome.ToUpper();
+            Tipo = tipo.ToUpper();
+            RegistroConselho = registroConselho.ToUpper();
+        }
+
+        public string SobreNome()
+        {
+            string[] lista = Nome.Split();
+
+            return lista[lista.Length - 1] + " - " + RegistroConselho;
+        }
+
+
+    }
+}
---
+++ C#/11. Aula 05.05/Exercicio 02/Triagem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_02
+{
+    internal class Triagem
+    {
+        public ProfissionalSaude Responsavel { get; set; }
+        public Paciente Paciente { get; set; } 
+
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+        public double PressãoArterial { get; set; }
+        public double Temperatura { get; set; }
+        public string Urgencia { get; set; }
+
+        public Triagem(ProfissionalSaude responsavel, Paciente paciente, double peso, double altura, double pressãoArterial, double temperatura, string urgencia)
+        {
+            Responsavel = responsavel;
+            Paciente = paciente;
+            Peso = peso;
+            Altura = altura;
+            PressãoArterial = pressãoArterial;
+            Temperatura = temperatura;
+            Urgencia = urgencia;
+        }
+
+    }
+}
