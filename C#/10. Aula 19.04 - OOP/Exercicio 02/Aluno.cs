64998dbfb430e03b42863cd5091e14dcf91f7ba9
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-08 17:02:18 -0300

    Adicionar exercicio 02 aula 10

---
+++ C#/10. Aula 19.04 - OOP/Exercicio 02/Aluno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_02
+{
+    internal class Aluno
+    {
+        public string nome;
+        public int matricula;
+        public DateOnly dataNascimento;
+        public int anoMatricula;
+        public Aluno(string nome, int matricula)
+        {
+            this.nome = nome;
+            this.matricula = matricula;
+            Console.WriteLine("Nome: " + nome + " e matricula: " + matricula);
+        }
+
+        public Aluno(DateOnly dataNascimento)
+        {
+            this.dataNascimento = dataNascimento;
+            Console.WriteLine("Data de nascimento: " + dataNascimento);
+        }
+
+        public Aluno(string nome, DateOnly dataNascimento, int anoMatricula)
+        {
+            this.nome = nome;
+            this.dataNascimento = dataNascimento;
+            this.anoMatricula = anoMatricula;
+            Console.WriteLine("Nome: " + nome + ", data de nascimento: " + dataNascimento+ " e ano que o aluno ingressou na faculdade: " +anoMatricula+".");
+        }
+
+    }
+}
--- C#/10. Aula 19.04 - OOP/Exercicio 02/Program.cs
+++ C#/10. Aula 19.04 - OOP/Exercicio 02/Program.cs
@@ -8,6 +8,18 @@
             //primeiro recebe o nome e a matrícula do aluno, o segundo recebe apenas a
             //data de nascimento do aluno e o terceiro construtor recebe o nome do aluno,
             //a data de nascimento e o ano em que o aluno ingressou na faculdade.
+
+            // Instanciação com o primeiro construtor
+            Aluno aluno1 = new Aluno("Daniela", 01258);
+
+            // Instanciação com o segundo construtor
+            Aluno aluno2 = new Aluno(new DateOnly(1996, 7, 4));
+
+            // Instanciação com o terceiro construtor
+            Aluno aluno3 = new Aluno("Maria", new DateOnly(1999, 6, 1), 2014);
+            
+            Console.ReadKey();
+
         }
     }
 }
