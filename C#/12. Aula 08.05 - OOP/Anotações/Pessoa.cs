28b639dd37613e1de04e87295ed7a59ae89a18c3
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-08 16:37:58 -0300

    Adicionar aula 12

---
+++ C#/12. Aula 08.05 - OOP/Anotações/Pessoa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anotações
+{
+    internal class Pessoa
+    {
+        private string nome; //atributos por padrão inicial por _nome
+        private int idade;
+        private double salario;
+
+        //public Pessoa(string nome, int idade)
+        //{
+        //    this.nome = nome;
+        //    this.idade = idade;
+        //    Console.WriteLine("Nome: "+nome+" - Idade: "+idade);
+        //}
+
+        //public Pessoa(int idade
+        //{
+        //    this.idade = idade;
+        //    Console.WriteLine("Idade " + idade);
+        //}
+
+        public Pessoa()
+        {
+            //this.nome = "Daniela";
+        }
+        public string Nome //C# trabalha com propriedade e por padrão começcam com maiusculo
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+
+
+    }
+}
---
+++ C#/12. Aula 08.05 - OOP/Anotações/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Anotações
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int idade;
+            //string nome;
+            //Pessoa p;
+
+            //Console.WriteLine("Digite o nome e idade: ");
+            //nome = Console.ReadLine();
+            //idade = int.Parse(Console.ReadLine());
+            //p = new Pessoa(nome, idade);
+
+            /* Para termos acesso a atributos privados, para atribuição e para acesso
+             * - acesso - queira usar esses atributos, ou apresentar 
+             A forma de ter acesso é utilizando os getters(para acesso) e os setters (para */
+
+            //Console.WriteLine("Nome: "+p.nome+" Salário: "+p.salario)
+
+            //Pessoa p;
+            //p = new Pessoa();
+            //Console.WriteLine("Digite o nome: ");
+            //string nome = Console.ReadLine();
+            //p.Nome = nome;
+
+            //string nomeRetornado;
+            //nomeRetornado = p.Nome;
+
+            //Console.WriteLine("Nome: "+nomeRetornado);
+
+            Carro c = new Carro();
+            c.Marca = "VW";
+            c.Ano = 1999;
+            c.Modelo = "Gol";
+            c.Motor = new Motor();
+            c.Motor.Cilindradas = 1500;
+            //c.Motor.Ligado = 1000;
+            
+            while(true)
+            {
+                Console.WriteLine("Digite 1 para ligar e 2 para desligar: ");
+                int op = int.Parse(Console.ReadLine());
+                if(op == 1)
+                {
+                    c.LigarCarro();
+                }
+                else if (op == 2)
+                {
+                    c.DesligarCarro();
+                }
+                else if(op == 0)
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
