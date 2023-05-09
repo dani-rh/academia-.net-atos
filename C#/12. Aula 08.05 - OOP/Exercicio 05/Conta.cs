5a7a82c813ea5bef1a7dcc059fd3d96d94b7cf69
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-09 16:50:54 -0300

    Adicionar exercicio 05 aula 12

---
+++ C#/12. Aula 08.05 - OOP/Exercicio 05/Conta.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_05
+{
+    internal class Conta
+    {
+        private string _numero;
+        private double _saldo;
+        private double _limite;
+
+        public Conta(string  numero, double saldo, double limite) 
+        {
+            _numero = numero;
+            _saldo = saldo;
+            _limite = limite;
+        }
+
+        public string Numero
+        {
+            get { return _numero; }
+        }
+
+        public double Saldo
+        {
+            get { return _saldo; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("O saldo não pode ser negativo.");
+                }
+                _saldo = value;
+            }
+        }
+
+        public double Limite
+        {
+            get { return _limite; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("O limite deve ser positivo.");
+                }
+                _limite = value;
+            }
+        }
+
+    }
+}
