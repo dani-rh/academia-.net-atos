ee6cc54e3aa6c40a741a41efb84a3fe888c513b7
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-17 18:33:03 -0300

    Adicionar exercicio 10 aula 12

---
+++ C#/12. Aula 08.05 - OOP/Exercicios/Exercicio 10/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_10
+{
+    internal class Animal
+    {
+        private string _nome;
+        private string _especie;
+        private int _idade;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+        public string Especie
+        {
+            get { return _especie; }
+            set { _especie = value; }
+        }
+        public int Idade
+        {
+            get { return _idade; }
+            set { _idade = value; }
+        }
+
+        public void EmitirSom()
+        {
+            Console.WriteLine("Som do animal");
+        }
+    }
+}
---
+++ C#/12. Aula 08.05 - OOP/Exercicios/Exercicio 10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //10 - Crie uma classe chamada Animal que tenha as propriedades Nome, Espécie e Idade. Crie também um método chamado
+            //EmitirSom, que imprime na tela o som do animal.
+
+            Animal animal = new Animal();
+            animal.Nome = "Leão";
+            animal.Especie = "Felino";
+            animal.Idade = 5;
+
+            Console.WriteLine("Nome: " + animal.Nome);
+            Console.WriteLine("Espécie: " + animal.Especie);
+            Console.WriteLine("Idade: " + animal.Idade);
+
+            animal.EmitirSom();
+
+        }
+    }
+}
