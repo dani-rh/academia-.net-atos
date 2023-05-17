745aa9f21b2801b69bbdc6a2c70020b6bba90008
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-17 17:49:08 -0300

    Adicionar exercicio 08 aula 12

---
+++ C#/12. Aula 08.05 - OOP/Exercicios/Exercicio 08/Aluno.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_08
+{
+    internal class Aluno
+    {
+        private string _nome;
+        private int _matricula;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("O nome do aluno não pode estar vazio.");
+                }
+                _nome = value;
+            }
+        }
+
+        public int Matricula
+        {
+            get { return _matricula; }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("A matrícula deve ser um valor positivo.");
+                }
+                _matricula = value;
+            }
+        }
+    }
+}
---
+++ C#/12. Aula 08.05 - OOP/Exercicios/Exercicio 08/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercicio_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //8 - Crie uma classe chamada Aluno que tenha as propriedades Nome e Matricula. Utilize o encapsulamento para garantir 
+            //que o nome não seja vazio(utilize função nativa) e que a matrícula seja positiva.
+
+            Aluno aluno = new Aluno();
+
+            try
+            {
+                Console.WriteLine("Digite o nome do aluno: ");
+                aluno.Nome = Console.ReadLine();
+
+                Console.WriteLine("Digite a matrícula do aluno: ");
+                aluno.Matricula = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Nome do aluno: "+aluno.Nome);
+                Console.WriteLine("Matrícula do aluno: "+aluno.Matricula);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: "+ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
