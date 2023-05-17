05f1511c008f8d79811849d6108a798eceea6341
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-17 18:41:56 -0300

    Adicionar exercicio 11 aula 12

---
+++ C#/12. Aula 08.05 - OOP/Exercicios/Exercicio 11/Aluno.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_11
+{
+    internal class Aluno
+    {
+        private string nome;
+        private double nota1;
+        private double nota2;
+        private double nota3;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public double Nota1
+        {
+            get { return nota1; }
+            set { nota1 = value; }
+        }
+
+        public double Nota2
+        {
+            get { return nota2; }
+            set { nota2 = value; }
+        }
+
+        public double Nota3
+        {
+            get { return nota3; }
+            set { nota3 = value; }
+        }
+
+        public double CalcularMedia()
+        {
+            return (nota1 + nota2 + nota3) / 3;
+        }
+
+        public string VerificarAprovacao()
+        {
+            if (CalcularMedia() >= 6)
+            {
+                return "Aprovado";
+            }
+            else
+            {
+                return "Reprovado";
+            }
+        }
+    }
+}
---
+++ C#/12. Aula 08.05 - OOP/Exercicios/Exercicio 11/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercicio_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //11 - Crie uma classe "Aluno" com os atributos "nome", "nota1", "nota2" e "nota3". Os atributos devem ser privados e
+            //acessados através de propriedades. Em seguida, crie um método para calcular a média e verificar se o aluno está aprovado
+            //ou reprovado(se a nota for maior ou igual a 6, está aprovado, senão está reprovado).
+
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Digite o nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a nota 1: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota 2: ");
+            aluno.Nota2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota 3: ");
+            aluno.Nota3 = double.Parse(Console.ReadLine());
+
+            double media = aluno.CalcularMedia();
+            string status = aluno.VerificarAprovacao();
+            
+            Console.WriteLine("O aluno " + aluno.Nome+ " obteve a média "+media+ " e está "+status+".");
+
+            Console.ReadKey();
+        }
+    }
+}
