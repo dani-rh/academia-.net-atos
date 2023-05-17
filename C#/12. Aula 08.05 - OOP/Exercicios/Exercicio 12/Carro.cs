770e76db2a0130169e914b99e7635df990b10990
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-17 19:01:54 -0300

    Adicionar exercicio 12 aula 12

---
+++ C#/12. Aula 08.05 - OOP/Exercicios/Exercicio 12/Carro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_12
+{
+    internal class Carro
+    {
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public int Velocidade { get; set; }
+
+        public void Acelerar()
+        {
+            Velocidade += 10;
+        }
+
+        public void Frear()
+        {
+            if (Velocidade >= 10)
+                Velocidade -= 10;
+            else
+                Velocidade = 0;
+        }
+    }
+}
---
+++ C#/12. Aula 08.05 - OOP/Exercicios/Exercicio 12/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //12 - Crie uma classe "Carro" com os atributos "modelo", "ano" e "velocidade". Em seguida, crie um método para acelerar o carro
+            //(aumentando a velocidade em 10) e outro para frear o carro(diminuindo a velocidade em 10, mas nunca deixando a
+            //velocidade negativa).
+
+            Carro meuCarro = new Carro();
+            meuCarro.Modelo = "Fusca";
+            meuCarro.Ano = 2020;
+
+            Console.WriteLine("Velocidade inicial: " + meuCarro.Velocidade);  // Velocidade inicial: 0
+
+            meuCarro.Acelerar();
+            Console.WriteLine("Velocidade após acelerar: " + meuCarro.Velocidade);  // Velocidade após acelerar: 10
+
+            meuCarro.Frear();
+            Console.WriteLine("Velocidade após frear: " + meuCarro.Velocidade);  // Velocidade após frear: 0
+        }
+    }
+}
