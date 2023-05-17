87aa1a278251bf030a2c8bd9d51b44e658fcb2e8
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-17 17:07:11 -0300

    Atualizar

--- C#/10. Aula 19.04 - OOP/Exercicio 03/Program.cs
+++ C#/10. Aula 19.04 - OOP/Exercicio 03/Program.cs
@@ -6,7 +6,7 @@
         {
             //Exercício 03 - Crie uma classe ContaCorrente que obedeça à descrição abaixo: A classe possui o atributo saldo do tipo 
             //double e os métodos definirSaldoInicial, depositar e sacar.O método definirSaldoInicial deve atribuir o
-            //valor passado por parâmetro ao atribuito saldo O método depositar, deve adicionar o valor passado por
+            //valor passado por parâmetro ao atribuito saldo. O método depositar, deve adicionar o valor passado por
             //parâmetro ao atributo saldo O método sacar deve reduzir o valor passado por parâmetro do saldo já
             //existente Necessário verificar a condição de o valor do saldo ser insuficiente para o saque que se deseja
             //fazer.O valor de retorno deve ser true(verdadeiro) quando for possível realizar o saque e false(falso)
