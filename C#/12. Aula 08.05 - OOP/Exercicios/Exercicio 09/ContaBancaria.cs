520861eb28276e0176329f027e3d916c64720950
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-17 18:32:52 -0300

    Adicionar exercicio 09 aula 12

---
+++ C#/12. Aula 08.05 - OOP/Exercicios/Exercicio 09/ContaBancaria.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_09
+{
+    internal class ContaBancaria
+    {
+        private string _numeroConta;
+        private string _titular;
+        private double _saldo;
+        private double _limite;
+
+        public string NumeroConta
+        {
+            get { return _numeroConta; }
+            set { _numeroConta = value; }
+        }
+        public string Titular
+        {
+            get
+            {
+                return _titular;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _titular = value.ToUpper();
+                }
+            }
+        }
+
+        public double Saldo
+        {
+            get { return _saldo; }
+            set { _saldo = value; }
+        }
+
+        public double Limite
+        {
+            get { return _limite; }
+            set
+            {
+                if(value > 0)
+                {
+                    _limite = value;
+                }
+            }
+        }
+
+        public void Depositar (double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo >= valor)
+            {
+                _saldo -= valor;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para saque!");
+                return false;
+            }
+        }
+        public double GetSaldo()
+        {
+            return Saldo;
+        }
+
+    }
+}
---
+++ C#/12. Aula 08.05 - OOP/Exercicios/Exercicio 09/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+
+namespace Exercicio_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 09 - Crie uma classe chamada ContaBancaria que tenha as propriedades NumeroConta, Titular, Saldo e Limite. Crie também
+            //um método chamado Depositar, que recebe um valor e adiciona ao saldo da conta, e um método Sacar, que recebe um
+            //valor e verifica se o saldo é suficiente para a operação.
+
+            ContaBancaria conta = new ContaBancaria();
+
+            conta.NumeroConta = "897-6";
+            conta.Titular = "Daniela Rodrigues Haeitmann";
+            conta.Saldo = 900;
+            conta.Limite = 500;
+
+            Console.WriteLine("Saldo: " +conta.Saldo);
+
+            Console.WriteLine("Digite o valor que será depositado: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+            conta.Depositar(valorDeposito);
+            Console.WriteLine("Saldo: "+conta.Saldo);
+
+            Console.WriteLine("Digite o valor que será sacado: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+            conta.Sacar(valorSaque);
+
+            Console.WriteLine("Saldo atual da conta: " + conta.GetSaldo());
+
+            Console.ReadKey();
+        }
+    }
+}
