28e56e86a6d3a7a954eed9c500a98b734c588176
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-17 19:02:06 -0300

    Adicionar exercicio 13 aula 12

---
+++ C#/12. Aula 08.05 - OOP/Exercicios/Exercicio 13/Livro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_13
+{
+    internal class Livro
+    {
+        public string Titulo { get; set; }
+        public Autor Autor { get; set; }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine("Título: " + Titulo);
+            Console.WriteLine("Autor(a): " + Autor.Nome);
+            Console.WriteLine("Nacionalidade do(a) autor(a): " + Autor.Nacionalidade);
+        }
+
+    }
+}
---
+++ C#/12. Aula 08.05 - OOP/Exercicios/Exercicio 13/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 13 - Crie uma classe autor. Em seguida, crie uma classe "Livro" com os atributos "titulo" e "autor". O atributo autor, deve ser 
+            //uma instancia de uma classe Autor. Crie um método para exibir as informações do livro.
+
+            Autor autor = new Autor();
+            autor.Nome = "Carla Madeira";
+            autor.Nacionalidade = "Brasileira";
+
+            Livro livro = new Livro();
+            livro.Titulo = "Tudo é rio";
+            livro.Autor = autor;
+
+            livro.ExibirInformacoes();
+        }
+    }
+}
