08f5d36704ae225466c0062a8a3225c29f45aa2f
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-22 14:38:24 -0300

    Atualizar

---
+++ C#/04. Aula 06.04/Exercicio 09/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Net;
+using System;
+
+namespace Exercicio_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //9 - Faça um programa no VS que receba do usuário 3 valores de glicemia em jejum, representando a medição
+            //diária de um paciente diabético. Para cada valor recebido analisar e informar ao paciente o seguinte:
+            //Se o valor diário for menor que 65, avisá - lo que corre risco de hipoglicemia
+            //Se o valor diário for maior que 250, avisá - lo que corre risco de hiperglicemia.
+
+            //Além disso, o programa deve fazer a média dos 3 valores de glicemia coletados.E se a média for
+            //menor que 80, avisá - lo que é preciso diminuir 2 unidades de insulina.Se a média for maior que
+            //150, avisá - lo que será necessário adicionar 2 unidades de insulina.
+
+
+        }
+    }
+}
---
+++ C#/15. Aula 15.05 - OOP/Exercicios/Exercicio 08/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Runtime.Intrinsics.X86;
+using System;
+
+namespace Exercicio_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercício 08 - Interface "IFormaGeometrica"
+            //Crie uma interface chamada "IFormaGeometrica" com dois métodos abstratos: "CalcularArea()" e "CalcularPerimetro()".
+            //Em seguida, crie duas classes que implementam essa interface: "Retangulo" e "Circulo".
+            //Implemente os métodos "CalcularArea()" e "CalcularPerimetro()" para cada uma dessas classes,
+            //de forma que o "Retangulo" calcule a área e o perímetro de um retângulo e o "Circulo" calcule a área e o perímetro de um círculo.
+            //Por fim, crie uma lista de "IFormaGeometrica" contendo um objeto "Retangulo" e um objeto "Circulo",
+            //e chame os métodos "CalcularArea()" e "CalcularPerimetro()" para cada um dos objetos.
+
+
+        }
+    }
+}
