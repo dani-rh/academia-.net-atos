cfa72ea1b06b18592cf7ac7e30020f4e42017aad
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-05-25 12:55:15 -0300

    Atualizar

--- C#/18. Aula 24.05 - Windows Form/Anotações/Form1.Designer.cs
+++ C#/18. Aula 24.05 - Windows Form/Anotações/Form1.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1056, 853);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
