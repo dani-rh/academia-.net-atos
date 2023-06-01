3adfc92616d4041335bd38db80bf93600ae0eafb
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-06-01 17:00:53 -0300

    Adicionar exercicio 01 aula 20

---
+++ C#/20. Aula 30.05 - Criptografia/Exercicio 01/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Exercicio_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox_usuario = new TextBox();
+            textBox_senha = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(192, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(200, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Senha";
+            // 
+            // textBox_usuario
+            // 
+            textBox_usuario.Location = new Point(281, 47);
+            textBox_usuario.Name = "textBox_usuario";
+            textBox_usuario.Size = new Size(100, 23);
+            textBox_usuario.TabIndex = 2;
+            // 
+            // textBox_senha
+            // 
+            textBox_senha.Location = new Point(281, 93);
+            textBox_senha.Name = "textBox_senha";
+            textBox_senha.Size = new Size(100, 23);
+            textBox_senha.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox_senha);
+            Controls.Add(textBox_usuario);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox_usuario;
+        private TextBox textBox_senha;
+    }
+}
---
+++ C#/20. Aula 30.05 - Criptografia/Exercicio 01/Form1.cs
@@ -0,0 +1,10 @@
+namespace Exercicio_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
