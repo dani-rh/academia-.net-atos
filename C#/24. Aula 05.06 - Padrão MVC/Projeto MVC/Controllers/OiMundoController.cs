001b928a61b263db1cb0114ee0d6ce6a816451c2
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-06-05 16:47:58 -0300

    Adicionar aula 24

---
+++ C#/24. Aula 05.06 - Padrão MVC/Projeto MVC/Controllers/OiMundoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Projeto_MVC.Controllers
+{
+    public class OiMundoController : Controller
+    {
+        public IActionResult Index()
+        //public string Index()
+        {
+            return View();
+            //return "Apresente esta mensagem";
+        }
+
+        public string Welcome(string nome, int ID = 1)
+        {
+            //return "Estou no método Welcome do OiMundoController";
+            //return HtmlEncoder.Default.Encode($"Oi {nome}, vezes: {vezes}");
+            return HtmlEncoder.Default.Encode($"Oi {nome}, ID: {ID}");
+        }
+
+        public string Oi()
+        {
+            return "oi";
+        }
+    }
+}
