a1f113a59884a4e54a99422602fdcc90ed1799cb
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-06-07 18:46:05 -0300

    Adicionar exercicio 01 aula 26

---
+++ C#/26. Aula 07.06/Exercicios/Exercicio 01 - CodeFirst/CodeFirst - Console/DataModels/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst___Console.DataModels
+{
+    public class Aluno
+    {
+        public int matricula { get; set; }
+        public string nome { get; set; }
+
+        public virtual ICollection<Curso> cursos{ get; set; }
+    }
+}
---
+++ C#/26. Aula 07.06/Exercicios/Exercicio 01 - CodeFirst/CodeFirst - Console/DataModels/Curso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst___Console.DataModels
+{
+    public class Curso
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+
+        public virtual Aluno aluno{ get; set; }
+    }
+}
