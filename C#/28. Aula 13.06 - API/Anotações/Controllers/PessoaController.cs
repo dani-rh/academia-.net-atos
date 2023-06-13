17f020175f7d6036ca0ee496544db071ef65e163
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-06-13 16:00:32 -0300

    Adicionar aula 28

---
+++ C#/28. Aula 13.06 - API/Anotações/Controllers/PessoaController.cs
@@ -0,0 +1,121 @@
+﻿using Anotações.DataModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+
+namespace Anotações.Controllers
+{
+    public class PessoaController : ControllerBase
+    { 
+        [HttpGet]
+        [Route("pessoas")]
+        
+        public async Task <IActionResult> getAllAsync(
+            [FromServices] Contexto contexto)
+        {
+            var pessoas = await contexto
+                .Pessoas
+                .AsNoTracking() //só pode ser utilizado em consultas - altamente recomendado
+                .ToListAsync();
+
+            return pessoas == null ? NotFound() : Ok(pessoas);
+        }
+
+        [HttpGet]
+        [Route("pessoas/{id}")]
+
+        public async Task<IActionResult> getByIdAsync(
+            [FromServices] Contexto contexto, [FromRoute] int id)
+        {
+            var pessoa = await contexto
+                .Pessoas.AsNoTracking()
+                .FirstOrDefaultAsync(p => p.id == id);
+
+            return pessoa == null ? NotFound() : Ok(pessoa);
+        }
+
+        [HttpPost]
+        [Route("pessoas")]
+
+        public async Task<IActionResult> PostAsync(
+            [FromServices] Contexto contexto, [FromBody] Pessoa pessoa)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await contexto.Pessoas.AddAsync(pessoa);
+                await contexto.SaveChangesAsync();
+                return Created($"pessoas/pessoas/{pessoa.id}", pessoa);
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("pessoas/{id}")]
+
+        public async Task<IActionResult> PutAsync
+            (
+                [FromServices] Contexto contexto,
+                [FromBody] Pessoa pessoa,
+                [FromRoute] int id
+            )
+        {
+            if (!ModelState.IsValid) //Verifica os atributos
+                return BadRequest("Model inválida");
+
+            var p = await contexto.Pessoas
+                .FirstOrDefaultAsync(x => x.id == id);
+
+            if (p == null)
+                return NotFound("Pessoa não encontrada!");
+
+            try
+            {
+                p.nome = pessoa.nome;
+
+                contexto.Pessoas.Update(p);
+                await contexto.SaveChangesAsync();
+                return Ok(p);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("pessoas/{id}")]
+        public async Task<IActionResult> DeleteAsync(
+            [FromServices] Contexto contexto,
+            [FromRoute] int id)
+        {
+            var p = await contexto.Pessoas.FirstOrDefaultAsync(x => x.id == id);
+
+            if (p == null)
+                return BadRequest("Pessoa não encontrada");
+            try
+            {
+                contexto.Pessoas.Remove(p);
+                await contexto.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    
+
+ 
+    }
+}
---
+++ C#/28. Aula 13.06 - API/Anotações/DataModels/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Anotações.DataModels
+{
+    public class Pessoa
+    {
+        
+        public int id { get; set; }
+        public string nome { get; set; }
+        
+        [JsonIgnore]
+
+        public virtual ICollection<Email>? emails { get; set; }
+    }
+}
