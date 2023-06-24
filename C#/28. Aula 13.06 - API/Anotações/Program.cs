92bdcafb4f2217b1536dca6944b57f19c80d1c1c
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-06-24 01:35:08 -0300

    Atualizar anotacao

--- C#/28. Aula 13.06 - API/Anotações/Program.cs
+++ C#/28. Aula 13.06 - API/Anotações/Program.cs
@@ -5,14 +5,28 @@ namespace Anotações
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
+
             builder.Services.AddSwaggerGen();
             builder.Services.AddDbContext<Contexto>();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                    policy =>
+                    {
+                        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                    });
+
+            }
+                
+           );
 
             var app = builder.Build();
 
@@ -29,6 +43,8 @@ namespace Anotações
 
 
             app.MapControllers();
+
+            app.UseCors(MyAllowSpecificOrigins); //ADD Politica de segurança
 
             app.Run();
         }
