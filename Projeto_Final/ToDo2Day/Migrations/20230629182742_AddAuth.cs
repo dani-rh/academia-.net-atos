c9443ae8cc9510419bc3c065d62b1d8d3902525b
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-06-29 16:11:19 -0300

    Adicionar Auth Migration

---
+++ Projeto_Final/ToDo2Day/Migrations/20230629182742_AddAuth.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo2Day.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "Users");
+        }
+    }
+}
