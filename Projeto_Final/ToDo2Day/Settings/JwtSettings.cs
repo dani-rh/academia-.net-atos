da39ddfae5f4f2caea64f493f7d33b751d71f2b2
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-06-29 16:12:59 -0300

    Adicionar JWTSettings

---
+++ Projeto_Final/ToDo2Day/Settings/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo2Day.Settings
+{
+    public class JwtSettings
+    {
+        public string SecretKey { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
