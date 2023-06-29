d14e14c73f8a254cd684495081d2eaef335372f8
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-06-29 16:19:17 -0300

    Adicionar servicos de autenticacao

--- Projeto_Final/ToDo2Day/Program.cs
+++ Projeto_Final/ToDo2Day/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 using ToDo2Day.Repository;
 using ToDo2Day.DataModels;
+using ToDo2Day.Settings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace ToDo2Day
 {
@@ -11,6 +16,29 @@ namespace ToDo2Day
         {
             var builder = WebApplication.CreateBuilder(args);
             var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+            //Carrega as configuracoes JWT
+            builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+
+            //Adiciona servicos de autenticacao e configura o portador JWT
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                // Define opcoes para validacao de token
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                    ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]))
+                };
+            });
+
+            // Add authorization services
+            builder.Services.AddAuthorization();
 
             // Add services to the container.
             builder.Services.AddScoped<ITagRepository, TagRepository>();
