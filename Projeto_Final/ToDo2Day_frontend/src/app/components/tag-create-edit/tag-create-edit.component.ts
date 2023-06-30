c116e89cff20499a8403b0bbabd35706267028da
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-06-30 19:34:22 -0300

    Adicionar componente tag-create-edit

---
+++ Projeto_Final/ToDo2Day_frontend/src/app/components/tag-create-edit/tag-create-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-create-edit',
+  templateUrl: './tag-create-edit.component.html',
+  styleUrls: ['./tag-create-edit.component.css']
+})
+export class TagCreateEditComponent {
+
+}
