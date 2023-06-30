ca39ae4c44c383bdb34052bd7e6eb16a66b5589d
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-06-30 19:35:05 -0300

    Atualizar componente task-create-edit

---
+++ Projeto_Final/ToDo2Day_frontend/src/app/components/task-create-edit/task-create-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-create-edit',
+  templateUrl: './task-create-edit.component.html',
+  styleUrls: ['./task-create-edit.component.css']
+})
+export class TaskCreateEditComponent {
+
+}
