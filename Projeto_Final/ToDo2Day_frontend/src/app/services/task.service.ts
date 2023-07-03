f34a851855e16ada2bf4c22bc45709b27717ea1d
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-07-03 13:39:25 -0300

    Atualizar task service

--- Projeto_Final/ToDo2Day_frontend/src/app/services/task.service.ts
+++ Projeto_Final/ToDo2Day_frontend/src/app/services/task.service.ts
@@ -8,44 +8,65 @@ import { AuthService } from './auth.service';
 import { environment } from 'environments/environment.dev';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
   private apiUrl = environment.apiUrl;
 
-  constructor(private httpClient: HttpClient, private authService: AuthService) { }
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService
+  ) {}
 
+  // Obtem opcoes HTTP incluindo cabecalhos. Inclui o token no cabecalho de autorizacao
   getHttpOptions() {
     return {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${this.authService.getToken()}`
-      })
+        Authorization: `Bearer ${this.authService.getToken()}`,
+      }),
     };
   }
 
+  // Busca todas as tasks
   getTasks(): Observable<TaskItemResponseDTO[]> {
     const url = `${this.apiUrl}/api/TaskItem`;
-    return this.httpClient.get<TaskItemResponseDTO[]>(url, this.getHttpOptions());
+    // Envia requisicao GET para buscar tarefas
+    return this.httpClient.get<TaskItemResponseDTO[]>(
+      url,
+      this.getHttpOptions()
+    );
   }
 
+  // Busca uma task por ID
   getTask(id: string): Observable<TaskItemResponseDTO> {
     const url = `${this.apiUrl}/api/TaskItem/${id}`;
+    // Envia requisicao GET para buscar a tarefa
     return this.httpClient.get<TaskItemResponseDTO>(url, this.getHttpOptions());
   }
 
+  // Cria uma nova task
   addTask(task: TaskItemCreateDTO): Observable<TaskItemResponseDTO> {
     const url = `${this.apiUrl}/api/TaskItem`;
-    return this.httpClient.post<TaskItemResponseDTO>(url, task, this.getHttpOptions());
-}
+    // Envia requisicao POST para criar tarefa
+    return this.httpClient.post<TaskItemResponseDTO>(
+      url,
+      task,
+      this.getHttpOptions()
+    );
+  }
 
+  // Atualiza uma task existente
   updateTask(id: string, task: TaskItemUpdateDTO): Observable<any> {
     const url = `${this.apiUrl}/api/TaskItem/${id}`;
+    // Envia requisicao PUT para atualizar a tarefa
     return this.httpClient.put(url, task, this.getHttpOptions());
   }
 
+  // Exclui uma task
   deleteTask(id: string): Observable<any> {
     const url = `${this.apiUrl}/api/TaskItem/${id}`;
+    // Envia requisicao DELETE para excluir uma task
     return this.httpClient.delete(url, this.getHttpOptions());
   }
 }
