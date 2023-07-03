5fc8564b40d62f26c1973107e62c4be4113c7d8f
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-07-03 13:36:57 -0300

    Atualizar register component

--- Projeto_Final/ToDo2Day_frontend/src/app/components/register/register.component.ts
+++ Projeto_Final/ToDo2Day_frontend/src/app/components/register/register.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { UserCreateDTO } from '../../dtos/user-create.dto';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
+  // Injetando servico de autenticacao e roteador
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    // Inicializando o grupo de formularios
+    this.registerForm = new FormGroup({
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.required),
+    });
+  }
+
+  onSubmit() {
+    //Se o formulario nao for valido, impede outras execucoes
+    if (!this.registerForm.valid) {
+      return;
+    }
+
+    const user: UserCreateDTO = this.registerForm.value; //Extrai os dados do usuario do form
+
+    // Registra o usuario
+    this.authService.register(user).subscribe((responseData) => {
+      console.log(responseData);
+      this.router.navigate(['/']);
+    });
+
+    this.registerForm.reset(); // Redefine o formulario
+  }
 }
