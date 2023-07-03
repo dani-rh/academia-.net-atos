a5963ba47145aa41b6b48cc0876e462da3d0ad35
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-07-03 13:39:17 -0300

    Atualizar tag service

--- Projeto_Final/ToDo2Day_frontend/src/app/services/tag.service.ts
+++ Projeto_Final/ToDo2Day_frontend/src/app/services/tag.service.ts
@@ -4,7 +4,6 @@ import { Tag } from '../models/tag.model';
 import { TagCreateDTO } from '../dtos/tag-create.dto';
 import { TagUpdateDTO } from '../dtos/tag-update.dto';
 import { environment } from 'environments/environment.dev';
-
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -15,32 +14,57 @@ export class TagService {
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
+  // Obtem o token do AuthService
   getToken() {
     return this.authService.getToken();
   }
 
+  // Busca todas as tags
   getAllTags() {
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.getToken()}`);
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Bearer ${this.getToken()}`
+    );
     return this.http.get<Tag[]>(`${this.baseUrl}/api/Tag`, { headers });
   }
 
+  // Busca uma tag por ID
   getTagById(id: string) {
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.getToken()}`);
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Bearer ${this.getToken()}`
+    );
     return this.http.get<Tag>(`${this.baseUrl}/api/Tag/${id}`, { headers });
   }
 
+  // Adiciona uma nova tag
   addTag(tagCreateDTO: TagCreateDTO) {
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.getToken()}`);
-    return this.http.post<Tag>(`${this.baseUrl}/api/Tag`, tagCreateDTO, { headers });
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Bearer ${this.getToken()}`
+    );
+    return this.http.post<Tag>(`${this.baseUrl}/api/Tag`, tagCreateDTO, {
+      headers,
+    });
   }
 
+  // Atualiza uma tag existente
   updateTag(id: string, tagUpdateDTO: TagUpdateDTO) {
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.getToken()}`);
-    return this.http.put(`${this.baseUrl}/api/Tag/${id}`, tagUpdateDTO, { headers });
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Bearer ${this.getToken()}`
+    );
+    return this.http.put(`${this.baseUrl}/api/Tag/${id}`, tagUpdateDTO, {
+      headers,
+    });
   }
 
+  // Exclui uma tag por ID
   deleteTag(id: string) {
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.getToken()}`);
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Bearer ${this.getToken()}`
+    );
     return this.http.delete(`${this.baseUrl}/api/Tag/${id}`, { headers });
   }
 }
