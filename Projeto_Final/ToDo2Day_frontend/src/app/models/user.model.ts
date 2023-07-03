3b0b4b48e88bfab5c46316f11b6b3ed2fb062ad7
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-07-02 21:40:08 -0300

    Adicionar models

---
+++ Projeto_Final/ToDo2Day_frontend/src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export interface User {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  refreshToken: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
