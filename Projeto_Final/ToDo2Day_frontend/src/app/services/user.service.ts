3f92d99505ef2dd9a19c38f79e517c62951ebe06
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-07-03 13:39:36 -0300

    Atualizar user service

--- Projeto_Final/ToDo2Day_frontend/src/app/services/user.service.ts
+++ Projeto_Final/ToDo2Day_frontend/src/app/services/user.service.ts
@@ -2,32 +2,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
-import { UserCreateDTO } from '../dtos/user-create.dto';
 import { UserUpdateDTO } from '../dtos/user-update.dto';
-import { UserLoginDTO } from '../dtos/user-login.dto';
 import { environment } from 'environments/environment.dev';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   private apiUrl = environment.apiUrl;
 
+  // Obtem todos os usuarios da API
   getAllUsers(): Observable<User[]> {
     return this.http.get<User[]>(`${this.apiUrl}/api/User`);
   }
 
+  // Ontem um usuario especifico por ID
   getUserById(id: string): Observable<User> {
     return this.http.get<User>(`${this.apiUrl}/api/User/${id}`);
   }
 
+  // Atualiza um usuario especifico por ID
   updateUser(id: string, user: UserUpdateDTO): Observable<void> {
     return this.http.put<void>(`${this.apiUrl}/api/User/${id}`, user);
   }
 
+  // Exclui um usuario especifico por ID
   deleteUser(id: string): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/api/User/${id}`);
   }
